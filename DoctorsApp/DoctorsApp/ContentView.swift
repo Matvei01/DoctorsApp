3f746ef33768d5345645484d979043dbf67a7d6e
Matvei <khlestov.matvei@mail.ru>
2024-07-28 22:27:57 +0500

    Added images for app

---
+++ DoctorsApp/DoctorsApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DoctorsApp
+//
+//  Created by Matvei Khlestov on 28.07.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            
+        }
+        .background(Color.gray)
+    }
+}
+
+#Preview {
+    ContentView()
+}
